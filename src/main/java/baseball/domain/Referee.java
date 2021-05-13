6982ce9f3e13e48d4de23d3d5970fc6c92fa49c5
Binsreoun <siryugubin@naver.com>
2021-05-13 16:33:18 +0900

    feat : 아웃 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -14,6 +14,10 @@ public class Referee {
             }
         }
         int ball = correctCount - strike;
+
+        if (strike == 0 && ball == 0){
+            return "아웃";
+        }
         return ball + "볼" + strike + "스트라이크";
     }
 }
